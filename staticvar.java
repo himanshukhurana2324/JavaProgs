c4047668bc4d1b7c99d1e42bbdb5da11b59a2ca3
Himanshu Khurana <126713985+himanshukhurana2324@users.noreply.github.com>
2023-11-09 22:56:15 +0530

    all set again

---
+++ staticvar.java
@@ -0,0 +1,16 @@
+class staticdemo {
+    private final static int count = 0;
+
+    static void show() {
+        // count++;
+        System.out.println(count);
+    }
+}
+
+class staticvar {
+    public static void main(String args[]) {
+        staticdemo.show();
+
+        // System.out.print?ln(s1.count);
+    }
+}
